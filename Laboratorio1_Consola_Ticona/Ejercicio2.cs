a74124e2142d1251155eff22f057fc39133fadc1
Jhon <jhoticonac@upt.pe>
2020-10-07 08:13:26 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio1_Consola_Ticona/Ejercicio2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1_Consola_Ticona
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Jhon";
+            string apellido = "Ticona";
+            int edad = 23;
+            double talla = 1.67;
+            bool sexo = true;
+
+            Console.WriteLine("Nombre : "+nombre);
+            Console.WriteLine("Apellido : " + apellido);
+            Console.WriteLine("Edad : " + edad);
+            Console.WriteLine("Estatura : " + talla);
+
+            if (sexo == true)
+            {
+                Console.WriteLine("Sexo : " + "M");
+            }
+            else
+            {
+                Console.WriteLine("Sexo : " + "F");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratorio1_Consola_Ticona/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1_Consola_Ticona
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //}
+    }
+}
